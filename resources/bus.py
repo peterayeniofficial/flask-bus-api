40e77ccf7f79c5f91a030a362273709781c2630c
Peter Ayeni <peter.ayeni@previsico.com>
2020-04-14 21:23:43 +0100

    added DELETE UPDATE GET

--- resources/bus.py
+++ resources/bus.py
@@ -11,4 +11,20 @@ class BusesApi(Resource):
     def post(self):
         body = request.get_json()
         bus = Bus(**body).save()
+        id = bus.id
+        return {'id': str(id)}, 200
+
+
+class BusApi(Resource):
+    def get(self, id):
+        bus = Bus.objects.get(id=id).to_json()
         return Response(bus, mimetype="application/json", status=200)
+
+    def put(self, id):
+        body = request.get_json()
+        Bus.objects.get(id=id).update(**body)
+        return '', 200
+
+    def delete(self, id):
+        Bus.objects.get(id=id).delete()
+        return '', 200
--- resources/routes.py
+++ resources/routes.py
@@ -1,5 +1,6 @@
-from .bus import BusesApi
+from .bus import BusesApi, BusApi
 
 
 def initialize_routes(api):
     api.add_resource(BusesApi, '/api/buses')
+    api.add_resource(BusApi, '/api/buses/<id>')
